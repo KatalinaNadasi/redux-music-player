1c2b446760ebcccb432e2e5ff86335c40dcbaa69
katalinanadasi <katalina.nadasi@gmail.com>
2020-02-12 21:50:02 +0100

    Updates

---
+++ precache-manifest.45cdef08942e6effab899560ce2a1599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0baeb40106c84a122948b9930991ef15",
+    "url": "/redux-music-player/index.html"
+  },
+  {
+    "revision": "54d96903a685b518a771",
+    "url": "/redux-music-player/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6cc21a6105c7c6dcc0a",
+    "url": "/redux-music-player/static/js/2.270e6612.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/redux-music-player/static/js/2.270e6612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d96903a685b518a771",
+    "url": "/redux-music-player/static/js/main.3430099a.chunk.js"
+  },
+  {
+    "revision": "87d3ddd1c3d187d3924a",
+    "url": "/redux-music-player/static/js/runtime-main.df7b571f.js"
+  }
+]);
